a303ea4702d14999c57dce2854a80616a983abe1
hafonium <giahao985@gmail.com>
2026-06-18 08:37:28 +0700

    Add HomeScreen, ProfileScreen, RegisterScreen and udpate LoginScreen, WelcomeScreen

--- app/src/main/java/com/example/thecodecup/ui/screens/Screen.kt
+++ app/src/main/java/com/example/thecodecup/ui/screens/Screen.kt
@@ -3,5 +3,7 @@ package com.example.thecodecup.ui.screens
 sealed class Screen(val route: String) {
     object Welcome : Screen("welcome_screen")
     object Login : Screen("login_screen")
+    object Register : Screen("register_screen")
     object Profile : Screen("profile_screen")
+    object Home : Screen("home_screen")
 }
--- app/src/main/java/com/example/thecodecup/ui/screens/ScreenNavigator.kt
+++ app/src/main/java/com/example/thecodecup/ui/screens/ScreenNavigator.kt
@@ -5,9 +5,11 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.thecodecup.ui.screens.home.HomeScreen
 import com.example.thecodecup.ui.screens.login.LoginScreen
+import com.example.thecodecup.ui.screens.profile.ProfileScreen
+import com.example.thecodecup.ui.screens.register.RegisterScreen
 import com.example.thecodecup.ui.screens.welcome.WelcomeScreen
-import com.example.thecodecup.utils.ScreenWrapper
 
 @Composable
 fun ScreenNavigator(
@@ -15,17 +17,45 @@ fun ScreenNavigator(
 ) {
     NavHost(navController = navController, startDestination = Screen.Welcome.route) {
 
+        val onNavigateToWelcome = {
+            navController.navigate(Screen.Welcome.route) {
+                popUpTo(0) {
+                    inclusive = true
+                }
+            }
+        }
+
         composable(route = Screen.Welcome.route) {
             WelcomeScreen(
-                onNavigateToLogin = { navController.navigate(Screen.Login.route) }
+                onNavigateToLogin = { navController.navigate(Screen.Login.route) },
+                onNavigateToRegister = { navController.navigate(Screen.Register.route) }
             )
         }
 
         composable(route = Screen.Login.route) {
             LoginScreen(
-                onNavigateToWelcome = { navController.popBackStack() }
+                onNavigateToWelcome = onNavigateToWelcome,
+                onNavigateToRegister = { navController.navigate(Screen.Register.route) }
             )
         }
 
+        composable(route = Screen.Register.route) {
+            RegisterScreen(
+                onNavigateToWelcome = onNavigateToWelcome,
+                onNavigateToLogin = { navController.navigate(Screen.Login.route) }
+            )
+        }
+
+        composable(route = Screen.Home.route) {
+            HomeScreen(
+                onNavigateToProfile = { navController.navigate(Screen.Login.route) },
+            )
+        }
+
+        composable(route = Screen.Profile.route) {
+            ProfileScreen(
+                onNavigateToHome = { navController.navigate(Screen.Home.route) },
+            )
+        }
     }
 }
